03e21aca4e84312c2680116884981c6a3f5a468e
Francisco Oliveto <franciscoliveto@gmail.com>
2025-03-26 12:11:02 -0300

    Now uses Wordnik API.

--- define.go
+++ define.go
@@ -2,7 +2,9 @@
 // Use of this source code is governed by a BSD-style
 // license that can be found in the LICENSE file.
 
-// Define uses the dictionaryapi.dev API from the command line to define its argument.
+// Define uses the Wordnik API from the command line to define its argument.
+// https://developer.wordnik.com/
+
 package main
 
 import (
@@ -15,55 +17,55 @@ import (
 	"os"
 )
 
-type Response struct {
-	Word      string
-	Phonetic  string
-	Phonetics []struct {
-		Text      string
-		Audio     string
-		SourceURL string
-		License   License
+type Definitions struct {
+	ID               string
+	PartOfSpeech     string
+	AttributionText  string
+	SourceDictionary string
+	Text             string
+	Sequence         string
+	Score            int
+	Word             string
+	ExampleUses      []struct {
+		Text     string
+		Position int
 	}
-	Meanings   []Meaning
-	License    License
-	SourceURLs []string
-}
-
-type Meaning struct {
-	PartOfSpeech string
-	Definitions  []struct {
-		Definition string
-		Synonyms   []string
-		Antonyms   []string
-		Example    string
+	AttributionURL string
+	WordnikURL     string
+	Labels         []struct {
+		Text string
+		Type string
 	}
-	Synonyms []string
-	Antonyms []string
-}
-
-type License struct {
-	Name string
-	URL  string
-}
-
-type Error struct {
-	Title      string
-	Message    string
-	Resolution string
-}
-
-var language = flag.String("language", "en", "dictionary language (two-letter code)")
-
-func main() {
-	log.SetFlags(0)
-	flag.Parse()
-	if len(flag.Args()) == 0 {
-		fmt.Fprintln(os.Stderr, "Usage: define [options] word")
-		flag.PrintDefaults()
-		os.Exit(1)
+	Citations []struct {
+		Source string
+		Cite   string
 	}
-	word := flag.Arg(0)
-	url := "https://api.dictionaryapi.dev/api/v2/entries/" + url.PathEscape(*language) + "/" + url.PathEscape(word)
+	Notes        []struct{}
+	RelatedWords []struct{}
+	TextProns    []struct{}
+}
+
+type Pronunciations struct {
+	ID              string
+	Raw             string
+	RawType         string
+	Seq             int
+	AttributionURL  string
+	AttributionText string
+}
+
+var key = flag.String("key", "", "Wordnik API key (defaults to $WORDNIKAPIKEY)")
+
+func getWord(word string, object string, values url.Values, v any) error {
+	url := "https://api.wordnik.com/v4/word.json/" + url.PathEscape(word) + "/" + object + "?" + values.Encode()
+	err := get(url, v)
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+func get(url string, v any) error {
 	resp, err := http.Get(url)
 	if err != nil {
 		log.Fatal(err)
@@ -71,33 +73,59 @@ func main() {
 	defer resp.Body.Close()
 	dec := json.NewDecoder(resp.Body)
 	if resp.StatusCode != http.StatusOK {
-		var e Error
-		if err := dec.Decode(&e); err != nil {
-			log.Fatal(err)
+		var e struct {
+			Message string
 		}
-		log.Fatalln(e.Title)
+		if err := dec.Decode(&e); err != nil {
+			return err
+		}
+		return fmt.Errorf("%s", e.Message)
 	}
-	var responses []Response
-	if err := dec.Decode(&responses); err != nil {
+	if err := dec.Decode(v); err != nil {
+		return err
+	}
+	return nil
+}
+
+func main() {
+	log.SetFlags(0)
+	flag.Parse()
+	if len(flag.Args()) == 0 {
+		fmt.Fprintln(os.Stderr, "Usage: define [options] <word>")
+		flag.PrintDefaults()
+		os.Exit(1)
+	}
+	if *key == "" {
+		*key = os.Getenv("WORDNIKAPIKEY")
+	}
+	if *key == "" {
+		log.Fatal("$WORDNIKAPIKEY not set")
+	}
+	v := make(url.Values)
+	v.Set("api_key", *key)
+	v.Set("sourceDictionaries", "ahd-5")
+	word := flag.Arg(0)
+	var defs []Definitions
+	if err := getWord(word, "definitions", v, &defs); err != nil {
 		log.Fatal(err)
 	}
-	var phonetic string
+	var prons []Pronunciations
+	if err := getWord(word, "pronunciations", v, &prons); err != nil {
+		log.Fatal(err)
+	}
+
 	m := make(map[string][]string)
-	for _, r := range responses {
-		word = r.Word
-		phonetic = r.Phonetic
-		for _, meaning := range r.Meanings {
-			s := meaning.PartOfSpeech
-			for _, d := range meaning.Definitions {
-				m[s] = append(m[s], d.Definition)
-			}
+	for _, d := range defs {
+		s := d.PartOfSpeech
+		if d.Text != "" {
+			m[s] = append(m[s], d.Text)
 		}
 	}
-	fmt.Printf("%s %s\n", word, phonetic)
-	for speech, definitions := range m {
+	fmt.Printf("%s /%s/\n", word, prons[0].Raw)
+	for speech, texts := range m {
 		fmt.Printf("\n%s\n", speech)
-		for i, d := range definitions {
-			fmt.Printf("%d. %s\n", i+1, d)
+		for i, t := range texts {
+			fmt.Printf("%d. %s\n", i+1, t)
 		}
 	}
 }
